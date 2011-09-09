0949be89c8056842879fdb482a836197c096dda6
SwearWord <swearword@blockface.org>
2011-09-09 14:19:52 -0400

    Package refactoring.

--- src/org/blockface/bukkitstats/CallHome.java
+++ src/org/blockface/bukkitstats/CallHome.java
@@ -23,7 +23,7 @@
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package blockface.stats;
+package org.blockface.bukkitstats;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.config.Configuration;
@@ -46,7 +46,7 @@ public class CallHome{
         if(cfg==null)verifyConfig();
         if(cfg.getBoolean("opt-out",false)) return;
         plugin.getServer().getScheduler().scheduleAsyncRepeatingTask(plugin,new CallTask(plugin,cfg.getBoolean("list-server",true)),0L,20L*60L*10);
-        System.out.println(plugin.getDescription().getName() + " is keeping usage stats. To opt-out for whatever bizarre reason, check plugins/stats.");
+        System.out.println(plugin.getDescription().getName() + " is keeping usage stats an. To opt-out for whatever bizarre reason, check plugins/stats.");
 
     }
 
